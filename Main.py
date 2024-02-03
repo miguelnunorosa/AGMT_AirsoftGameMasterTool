0e7c9f53b25a073012509c062cd5801f34adb38b
Miguel <mike@gmail.com>
2024-02-03 21:48:38 +0000

    create submenu players

--- Main.py
+++ Main.py
@@ -14,13 +14,18 @@ class Main:
 
         # Adicionar opções ao menu
         self.menu_opcoes = tk.Menu(self.menu_principal, tearoff=0)
-        self.menu_opcoes.add_command(label="Definições", command=self.abrir_definicoes)
-        self.menu_opcoes.add_separator()
-        self.menu_opcoes.add_command(label="Sair", command=self.sair)
         self.menu_principal.add_cascade(label="Menu", menu=self.menu_opcoes)
 
-    def abrir_definicoes(self):
-        messagebox.showinfo("Definições", "Aqui você pode configurar suas definições.")
+        # Adicionar submenu "Definições" ao menu "Menu"
+        self.submenu_definicoes = tk.Menu(self.menu_opcoes, tearoff=0)
+        self.submenu_definicoes.add_command(label="Jogadores", command=self.abrir_jogadores)
+        self.menu_opcoes.add_cascade(label="Definições", menu=self.submenu_definicoes)
+
+        self.menu_opcoes.add_separator()
+        self.menu_opcoes.add_command(label="Sair", command=self.sair)
+
+    def abrir_jogadores(self):
+        messagebox.showinfo("Jogadores", "Aqui você pode gerenciar jogadores.")
 
     def sair(self):
         if messagebox.askokcancel("Sair", "Tem certeza que deseja sair?"):
