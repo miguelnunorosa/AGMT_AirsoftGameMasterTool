e5af625ee7c872a647bb2579a177dbbfa066b101
Miguel <mike@gmail.com>
2024-02-04 11:57:19 +0000

    CRUD players

--- ScreenPlayers.py
+++ ScreenPlayers.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 import sqlite3
+import os
 
 class InterfaceJogadores:
     def __init__(self, root):
@@ -9,37 +10,32 @@ class InterfaceJogadores:
         self.root.geometry("400x400")
         self.root.resizable(False, False)
 
-        # Criar o menu
-        self.menu_principal = tk.Menu(self.root)
-        self.root.config(menu=self.menu_principal)
+        # Criar a barra de menus
+        self.barra_menus = tk.Menu(self.root)
+        self.root.config(menu=self.barra_menus)
 
-        # Adicionar opções ao menu
-        self.menu_opcoes = tk.Menu(self.menu_principal, tearoff=0)
-        self.menu_principal.add_cascade(label="Menu", menu=self.menu_opcoes)
+        # Adicionar opções ao menu "Menu"
+        self.menu_opcoes = tk.Menu(self.barra_menus, tearoff=0)
+        self.barra_menus.add_cascade(label="Menu", menu=self.menu_opcoes)
 
-        # Adicionar submenu "Definições" ao menu "Menu"
-        self.submenu_definicoes = tk.Menu(self.menu_opcoes, tearoff=0)
-        self.submenu_definicoes.add_command(label="Jogadores", command=self.abrir_jogadores)
-        self.menu_opcoes.add_cascade(label="Adicionar", menu=self.submenu_definicoes)
+        # Adicionar submenu "Jogador" ao menu "Menu"
+        self.submenu_jogador = tk.Menu(self.menu_opcoes, tearoff=0)
+        self.submenu_jogador.add_command(label="Adicionar", command=self.adicionar_jogador)
+        self.submenu_jogador.add_command(label="Editar", command=self.editar_jogador, state="disabled")
+        self.submenu_jogador.add_command(label="Excluir", command=self.excluir_jogador, state="disabled")
+        self.menu_opcoes.add_cascade(label="Jogador", menu=self.submenu_jogador)
 
-        self.menu_opcoes.add_separator()
+        # Adicionar opção "Voltar" ao menu "Menu"
         self.menu_opcoes.add_command(label="Voltar", command=self.sair)
 
-        # Variáveis para entrada de dados
-        self.nome_var = tk.StringVar()
+        # Variável para armazenar jogadores
+        self.jogadores = []
 
-        # Frame para entrada de dados
-        self.frame_entrada = tk.Frame(self.root)
-        self.frame_entrada.pack(pady=20)
+        # Label no centro da interface
+        self.label_titulo = tk.Label(self.root, text="Jogadores", font=("Helvetica", 16))
+        self.label_titulo.pack(pady=10)
 
-        tk.Label(self.frame_entrada, text="Nome do Jogador:").grid(row=0, column=0, padx=10, pady=10)
-        entry_nome = tk.Entry(self.frame_entrada, textvariable=self.nome_var)
-        entry_nome.grid(row=0, column=1, padx=10, pady=10)
-
-        # Botão para adicionar jogador
-        tk.Button(self.frame_entrada, text="Adicionar Jogador", command=self.registrar_novo_jogador).grid(row=1, column=0, columnspan=2, pady=10)
-
-        # Widget Listbox para exibir jogadores
+        # Listbox para exibir jogadores
         self.listbox_jogadores = tk.Listbox(self.root, width=40, height=10)
         self.listbox_jogadores.pack(pady=20)
 
@@ -49,50 +45,74 @@ class InterfaceJogadores:
         # Exibir jogadores já cadastrados ao iniciar a interface
         self.atualizar_lista_jogadores()
 
-    def abrir_jogadores(self):
-        messagebox.showinfo("Airsoft Game Master - Gestão Jogadores", "Aqui você pode gerenciar jogadores.")
+    def atualizar_lista_jogadores(self):
+        self.listbox_jogadores.delete(0, tk.END)  # Limpar a lista atual
+        self.jogadores.clear()
 
-    def sair(self):
-        self.root.destroy()
+        # Verificar se o banco de dados existe
+        if not os.path.exists("settings/players.db"):
+            return
 
-    def registrar_novo_jogador(self):
-        nome_jogador = self.nome_var.get()
+        conn = sqlite3.connect("settings/players.db")
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS jogadores (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL)")
+        cursor.execute("SELECT * FROM jogadores")
+        jogadores = cursor.fetchall()
 
+        for jogador in jogadores:
+            self.jogadores.append(jogador[1])
+            self.listbox_jogadores.insert(tk.END, jogador[1])
+
+        conn.close()
+
+    def adicionar_jogador(self):
+        nome_jogador = simpledialog.askstring("Adicionar Jogador", "Digite o nome do jogador:")
         if nome_jogador:
-            conn = sqlite3.connect("jogadores.db")
+            conn = sqlite3.connect("settings/players.db")
             cursor = conn.cursor()
-            cursor.execute('''
-                CREATE TABLE IF NOT EXISTS jogadores (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nome TEXT NOT NULL
-                )
-            ''')
             cursor.execute("INSERT INTO jogadores (nome) VALUES (?)", (nome_jogador,))
             conn.commit()
             conn.close()
 
-            # Limpar a entrada após adicionar jogador
-            self.nome_var.set("")
-
             # Atualizar a lista de jogadores
             self.atualizar_lista_jogadores()
 
-    def atualizar_lista_jogadores(self):
-        self.listbox_jogadores.delete(0, tk.END)  # Limpar a lista atual
+    def editar_jogador(self):
+        selected_index = self.listbox_jogadores.curselection()
+        if selected_index:
+            jogador_atual = self.jogadores[selected_index[0]]
+            novo_nome = simpledialog.askstring("Editar Jogador", f"Novo nome para {jogador_atual}:")
+            if novo_nome:
+                conn = sqlite3.connect("settings/players.db")
+                cursor = conn.cursor()
+                cursor.execute("UPDATE jogadores SET nome = ? WHERE nome = ?", (novo_nome, jogador_atual))
+                conn.commit()
+                conn.close()
 
-        conn = sqlite3.connect("jogadores.db")
-        cursor = conn.cursor()
-        cursor.execute("SELECT nome FROM jogadores")
-        jogadores = cursor.fetchall()
+                # Atualizar a lista de jogadores
+                self.atualizar_lista_jogadores()
 
-        for jogador in jogadores:
-            self.listbox_jogadores.insert(tk.END, jogador[0])
+    def excluir_jogador(self):
+        selected_index = self.listbox_jogadores.curselection()
+        if selected_index:
+            jogador_atual = self.jogadores[selected_index[0]]
+            opcao = messagebox.askokcancel("Remover Jogador", f"Tem certeza que deseja remover o jogador {jogador_atual}?")
+            if opcao:
+                conn = sqlite3.connect("settings/players.db")
+                cursor = conn.cursor()
+                cursor.execute("DELETE FROM jogadores WHERE nome = ?", (jogador_atual,))
+                conn.commit()
+                conn.close()
 
-        conn.close()
+                # Atualizar a lista de jogadores
+                self.atualizar_lista_jogadores()
 
     def selecionar_jogador(self, event):
         selected_index = self.listbox_jogadores.curselection()
         if selected_index:
-            nome_jogador = self.listbox_jogadores.get(selected_index)
-            #messagebox.showinfo("Selecionar Jogador", f"Você selecionou o jogador: {nome_jogador}")
+            self.submenu_jogador.entryconfig("Editar", state="normal")
+            self.submenu_jogador.entryconfig("Excluir", state="normal")
+
+    def sair(self):
+        self.root.destroy()
 
