f72a9fd18b8d2066575d4d6cdf0a37fc3f65cb31
Miguel Rosa <miguelrosa.programacao@gmail.com>
2024-02-13 17:53:20 +0000

    update AGMT webservice

---
+++ RaspberryPi/webservice/Main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+
+app = Flask(__name__, template_folder='settings/templates')
+appTitle = 'Airsoft Game Master Tool - Results'
+
+
+
+
+@app.route('/jogadores') # Rota para a página de jogadores
+def jogadores():
+    conn = sqlite3.connect('settings/database/agmtdatabase.db')   # Conectar ao banco de dados
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM players")                       # Executar a query para obter os jogadores
+    jogadores = cursor.fetchall()                                 # Obter todos os jogadores
+    conn.close()                                                  # Fechar a conexão com o banco de dados
+    return render_template('jogadores.html', jogadores=jogadores) # Renderizar o template com os dados dos jogadores
+
+
+@app.route('/')
+def index():
+    return render_template('dashboard.html', title=appTitle)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("3000"), debug=True)
